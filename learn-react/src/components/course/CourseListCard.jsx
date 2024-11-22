adf8aea79b99983f87d8e3fe69217ca15bd2b37c
Nova <heetask@gmail.com>
2024-11-23 00:51:20 +0900

    JSX 에서 자바스크립트 사용

--- learn-react/src/components/course/CourseListCard.jsx
+++ learn-react/src/components/course/CourseListCard.jsx
@@ -1,9 +1,10 @@
 import CourseItem from "./CourseItem";
 
 export default function CourseListCard() {
+  const title = "강의 목록";
   return (
     <div className="card">
-      <div className="card__header">강의 목록</div>
+      <div className="card__header">{title}</div>
       <div className="card__body">
         <div className="courses">
           <CourseItem />
