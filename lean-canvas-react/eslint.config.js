6570a8146b6781027f87862b1f844c7a88171df0
Nova <56466589+nova-fe@users.noreply.github.com>
2024-12-06 12:15:13 +0000

    ESLint & Prettier 설정

--- lean-canvas-react/eslint.config.js
+++ lean-canvas-react/eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -35,4 +36,5 @@ export default [
       ],
     },
   },
-]
+  eslintConfigPrettier, // Prettier 설정을 마지막에 추가하여 스타일 충돌 방지
+];
