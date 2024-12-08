fe87d255dfc5496a48d78a1c4a702f0c7173855f
Nova <56466589+nova-fe@users.noreply.github.com>
2024-12-08 08:55:51 +0000

    Tailwind CSS

--- lean-canvas-react/src/main.jsx
+++ lean-canvas-react/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppStyled.jsx';
+import App from './AppTailwindCSS.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
