559acf4625d8b192693d7e53d9346c5c4a3c4026
Nova <56466589+nova-fe@users.noreply.github.com>
2024-12-18 08:36:42 +0000

    메모 추가 및 제거 UI

--- lean-canvas-react/src/components/CanvasCard.jsx
+++ lean-canvas-react/src/components/CanvasCard.jsx
@@ -1,6 +1,19 @@
 import { FaPlus } from 'react-icons/fa';
+import Note from './Note';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; // 고유키 생성
 
 export default function CanvasCard({ title, isSubtitle = false }) {
+  const [notes, setNotes] = useState([]);
+
+  const handleAddNote = () => {
+    setNotes([...notes, { id: uuidv4(), content: '' }]);
+  };
+
+  const handleDeleteNote = id => {
+    setNotes(notes.filter(item => item.id !== id));
+  };
+
   return (
     <div className="row-span-1 bg-white min-h-48 border border-collapse border-gray-300">
       <div
@@ -9,11 +22,25 @@ export default function CanvasCard({ title, isSubtitle = false }) {
         } flex items-start justify-between px-3 py-2`}
       >
         <h3 className={`${isSubtitle === false && 'font-bold'}`}>{title}</h3>
-        <button className="bg-blue-400  text-white p-1.5 text-xs rounded-md">
+        <button
+          className="bg-blue-400  text-white p-1.5 text-xs rounded-md"
+          onClick={() => {
+            handleAddNote();
+          }}
+        >
           <FaPlus />
         </button>
       </div>
-      <div className="space-y-3 min-h-32 p-3">memo..</div>
+      <div className="space-y-3 min-h-32 p-3">
+        {notes.map(note => (
+          <Note
+            key={note.id}
+            id={note.id}
+            content={note.content}
+            onDeleteNote={handleDeleteNote}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- lean-canvas-react/src/components/Header.jsx
+++ lean-canvas-react/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <header className="sticky top-0 bg-gray-800 text-white px-4">
+    <header className="sticky top-0 bg-gray-800 text-white px-4 z-30">
       <div className="container mx-auto flex justify-between items-center h-14">
         <div>
           <Link to="/" className="text-xl font-bold">
